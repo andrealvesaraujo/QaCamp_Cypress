52581b52b35ed407576bb96f2224dea9f8ce52bf
Andre Alves <andrealvesaraujo.1995@gmail.com>
2021-12-30 10:22:33 -0300

    Basic Script - Part 3

---
+++ cypress/integration/TC_01_basics.spec.js
@@ -0,0 +1,9 @@
+describe('First test', () => {
+    it('Login and Click', () => {
+        cy.visit("https://react-redux.realworld.io/#/login")
+        cy.get('input[type="email"]').type('test@gmail.com')
+        cy.get('input[type="password"]').type('12345')
+        cy.get('.btn').contains('Sign in').should('be.visible').click()
+    });
+})
+    
